f6fe435f3daf8a0c1f11e340a86f0a0259d4318f
JaysinH13 <90346481+JaysinH13@users.noreply.github.com>
2023-01-27 10:05:27 -0500

    Moved and Fixed
    Moved pogs into static folder.
    Had OnlyPogs.ejs made into Pog.ejs
    Pog.ejs has become a template page to load every pog
    Renamed Pogs to have them shown on the template based off the name

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ const path = require('path');
 const fs = require('fs');
 
 var app = express();
+var selectedPog;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('./static'));
 
@@ -14,9 +15,22 @@ let rawdata = fs.readFileSync('pogs.json')
 let poglist = JSON.parse(rawdata)
 console.log(poglist);
 
-app.get('/', function(req, res) {
-    res.render('index', { pog: poglist} );
-  });
+app.get('/', function (req, res) {
+  res.render('index', { pog: poglist });
+});
+app.get('/pog', function (req, res) {
+  console.log(req.query.name);
+  let pogName = req.query.name;
+  console.log(pogName);
+  for (let pogs of poglist.pogs) {
+    if (pogs.name === pogName) {
+      selectedPog = pogs;
+      break;
+    }
+  }
+  console.log(selectedPog);
+  res.render('pog', { pog: selectedPog });
+});
 
 
 app.listen(621);
