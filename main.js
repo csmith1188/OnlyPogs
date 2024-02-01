ed919db8b5856fe5e3c3b0019e6fd9ea7c58334f
Talon24229 <82524610+Talon24229@users.noreply.github.com>
2024-02-01 14:40:36 -0500

    Upload Pogs (Fully Functional)

--- main.js
+++ main.js
@@ -1,33 +1,29 @@
 //This is the OnlyPogs main.js file
 
-//requiring indepencies
+//requiring independencies
 const express = require('express')
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const path = require('path');
 const fs = require('fs');
-const { log } = require('console');
+const {
+  log
+} = require('console');
 const sqlite3 = require('sqlite3').verbose();
 const jwt = require('jsonwebtoken')
 const session = require('express-session')
 const app = express();
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-app.use(express.urlencoded({ extended: true }));
-
-app.use(cookieParser());
 
 //OnlyPogs Port
-const PORT = 6969
+const PORT = 3000
 
 //formbar.js url
-const AUTH_URL = 'http://172.16.3.145:1128/oauth'
+// const AUTH_URL = 'http://172.16.3.106:420/oauth'
 
-//OnlyPogs url
-
-const THIS_URL = 'http://172.16.3.102:6969/login'
-
-const THIS_URL = 'http://172.16.3.145:6969/login'
-
+// //OnlyPogs url
+// const THIS_URL = 'http://172.16.3.107:1024/login'
 
 const dbPath = path.join('./static', 'pog.db');
 
@@ -40,64 +36,28 @@ const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
   }
 });
 
-app.use(express.urlencoded({ extended: true }));
+
+/** A function that checks to see if there is a session token and if there is it redirects to the login endpoint*/
+// function isAuthenticated(req, res, next) {
+//   if (req.session.token) next()
+//   else res.redirect('/login')
+// };
+
+app.use(express.urlencoded({
+  extended: true
+}));
 
 app.use(express.static('./static'));
 
-app.use(session({
-  secret: 'D$jtDD_}g#T+vg^%}qpi~+2BCs=R!`}O',
-  resave: false,
-  saveUninitialized: false
-}))
+// app.use(session({
+//   secret: 'D$jtDD_}g#T+vg^%}qpi~+2BCs=R!`}O',
+//   resave: false,
+//   saveUninitialized: false
+// }))
 
-function isAuthenticated(req, res, next) {
-  if (req.session.token) next()
-  else res.redirect('/login')
-};
-
-const apiR = require('./api.js')
-
-app.use('/api', apiR(db))
 
 //Setting the view engine to look for ejs
 app.set('view engine', 'ejs');
-
-
-/**
- * This is an get endpoing that calls the isAuthenticated function when it runs
- */
-app.get('/', isAuthenticated, (req, res) => {
-  const userPerm = req.session.token.permissions
-  const userName = req.session.token.username
-  try {
-    db.all('SELECT * FROM pogs', [], (err, rows,) => {
-      //error handling
-      if (err) {
-        console.error(err);
-      } else {
-        res.render('index', { rows: rows, userPerm: userPerm, userName: userName })
-      }
-    });
-  }
-  catch (error) {
-    res.send(error.message);
-  }
-});
-/**
-Sends you to the /login endpoint
-Sets tokenData to the sessions token data
-Then redirects you do the root endpoint.
-*/
-app.get('/login', (req, res) => {
-  console.log(req.query)
-  if (req.query.token) {
-    var tokenData = jwt.decode(req.query.token);
-    req.session.token = tokenData;
-    res.redirect('/');
-  } else {
-    res.redirect(AUTH_URL + `?redirectURL=${THIS_URL}`);
-  };
-})
 
 /**get endpoint that takes you to the account.ejs page */
 app.get('/acc', (req, res) => {
@@ -105,114 +65,104 @@ app.get('/acc', (req, res) => {
     if (err) {
       console.log(err);
     };
-    res.render('account', { rows: rows })
+    res.render('account', {
+      rows: rows
+    })
   })
 })
+
 
 /**
  * The following function is a get endpoint that takes you to the rewards.ejs page, it then creates a variable and sets it to the permissions
  * There is a db.all to select all from the rewards table in the pogs database
-* There is a db.get to select all from the Digipogs table of the pog database
-* Then it renders the rewards page with the rows from the rewards table and the perms from the Digipogs table
-*/
+ * There is a db.get to select all from the Digipogs table of the pog database
+ * Then it renders the rewards page with the rows from the rewards table and the perms from the Digipogs table
+ */
 app.get('/rewards', (req, res) => {
-  const userPerm = req.session.token.permissions
-  console.log(userPerm)
-  // console.log(userPerm)
+  const digiPerm = req.query.permissions
   db.all('Select * FROM rewards', [], (err, rows) => {
     //error validation
     if (err) {
       console.log(err)
       //TODO: send error template here
+
     }
     db.get('SELECT * FROM Digipogs', [], (err,) => {
       //error validation
       if (err) {
         console.log(err)
       } else {
-        res.render('rewards', { rows: rows, userPerm: userPerm })
+        res.render('rewards', {
+          rows: rows
+        })
       }
     })
   })
 })
-app.use(bodyParser.json());
 
-app.post('/addItem', (req, res) => {
-  const item = req.body.item
-  const cost = req.body.cost
-  const type = req.body.type
-  const checkPerms = req.body.userPerm
-  console.log(`Uid: ${uid}`)
-  console.log(item)
-  console.log(cost)
-  console.log(type)
-  if (checkPerms == req.session.token.permissions) {
-    db.run('INSERT INTO rewards (item, cost, type) VALUES (?, ?, ?)', [item, cost, type], (err) => {
-      if (err) {
-        console.log(err);
-        //TODO: send error template here
-      } else {
-        res.redirect('/rewards')
-      }
-    });
-  } else {
-    // res.send("Insufficient Permissions")
-  }
-})
 
-app.post('/editItem', (req, res) => {
+app.post('/rewards', (req, res) => {
   const uid = req.body.uid
   const item = req.body.item
   const cost = req.body.cost
   const type = req.body.type
-  const checkPerms = req.body.userPerm
-
-  console.log(`Uid: ${uid}`)
-  console.log(item)
-  console.log(cost)
-  console.log(type)
-
-  if (checkPerms == req.session.token.permissions) {
-    db.run('UPDATE rewards SET item = ?, cost = ?, type = ? WHERE uid = ?', [item, cost, type, uid], (err) => {
-      if (err) {
-        console.log(err);
-        // TODO: send error template here
-      } else {
-        res.redirect('/rewards');
-      }
-    });
-  } else {
-    // res.send("Insufficient Permissions")
-  }
-})
-
-
-app.post('/deleteItem', (req, res) => {
-  const uid = req.body.uid
-  const checkPerms = req.body.userPerm
-  console.log(uid)
-  if (checkPerms == req.session.token.permissions) {
-    db.run('DELETE FROM rewards WHERE uid = ?', [uid], (err) => {
-      if (err) {
-        console.log(err);
-        // TODO: send error template here
-      } else {
-        res.redirect('/rewards');
-      }
-    });
-  } else {
-    // res.send("Insufficient Permissions")
-  }
+  db.run('INSERT INTO rewards (uid, item, cost, type) VALUES (?, ?, ?, ?)', [uid, item, cost, type], (err) => {
+    if (err) {
+      console.log(err);
+      //TODO: send error template here
+    } else {
+      res.redirect('/rewards')
+    }
+  });
 })
 
 app.get('/rDetails', (req, res) => {
   res.render('rewardsDetails.ejs')
 })
 
+/**
+ * This is an get endpoing that calls the isAuthenticated function when it runs
+ */
+// isAuthenticated
+app.get('/', (req, res) => {
+  // const userPerm = req.session.token.permissions
+  // const userName = req.session.token.username
+  try {
+    db.all('SELECT * FROM pogs', [], (err, rows,) => {
+      //error handling
+      if (err) {
+        console.error(err);
+      }
+      res.render('index', {
+        rows: rows
+      })
+    });
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+
+/**
+Sends you to the /login endpoint
+Sets tokenData to the sessions token data
+Then redirects you do the root endpoint.
+*/
+// app.get('/login', (req, res) => {
+//   if (req.query.token) {
+//     var tokenData = jwt.decode(req.query.token);
+//     req.session.token = tokenData;
+
+//     res.redirect('/');
+//   } else {
+//     res.redirect(AUTH_URL + `?redirectURL=${THIS_URL}`);
+//   };
+// });
+
+
 app.get('/pog', function (req, res) {
   const pogName = req.query.name;
   const parentID = req.query.parentID;
-  const userPerm = req.session.token.permissions;
+  // const userPerm = req.session.token.permissions;
   // Use Promises for better error handling and parallel execution
   Promise.all([
     new Promise((resolve, reject) => {
@@ -221,15 +171,15 @@ app.get('/pog', function (req, res) {
           // Handle the case where no data was found for the given name
           res.status(404).send('Pog not found');
           return;
-         }
-         
-         let colors;
-         try {
+        }
+
+        let colors;
+        try {
           colors = JSON.parse(row.color).colors;
-         } catch (error) {
+        } catch (error) {
           console.error('Error parsing JSON:', error, row.color);
-         }
-         row.colors = colors;
+        }
+        row.colors = colors;
 
         resolve(row);
       });
@@ -252,7 +202,8 @@ app.get('/pog', function (req, res) {
 
     .then(([pogData, colorData,]) => {
       // Both queries have completed successfully
-      res.render('pog', { pog: pogData, color: colorData, userPerm: userPerm });
+      // res.render('pog', { pog: pogData, color: colorData, userPerm: userPerm });
+      res.render('pog', { pog: pogData, color: colorData });
       console.log(colorData)
       console.log(pogData);
     })
@@ -262,23 +213,54 @@ app.get('/pog', function (req, res) {
     })
 })
 
-app.post('/savePog', (req, res) => {
+app.post('/savePog', function (req, res) {
   const color = req.body.color;
   const amount = req.body.amount;
   const serial = req.body.serial;
   const tags = req.body.tags;
   const lore = req.body.lore;
   const uid = req.body.uid;
-  
-  // Log the received data
-  console.log('Received data:', {
-    color: color,
-    amount: amount,
-    serial: serial,
-    tags: tags,
-    lore: lore,
-    uid: uid
-  });
+  const uploadedImages = req.body.uploadedImages; // Get the uploadedImages object from the body of the POST request
+  console.log(req.body);
+ 
+  // Iterate over the uploadedImages object
+  for (const imageName in uploadedImages) {
+     if (uploadedImages.hasOwnProperty(imageName)) {
+       let imageData = uploadedImages[imageName];
+ 
+       // Remove the data URL prefix
+       imageData = imageData.toString().replace(/^data:image\/[^;]+;base64,/, "");
+ 
+       // Convert the base64 string back to binary data
+       const buffer = Buffer.from(imageData, 'base64');
+ 
+       // Define the path where the image will be saved
+       const filePath = path.join(__dirname, 'static', 'pogs', `${imageName}.png`);
+ 
+       // Write the binary data to a file
+       fs.writeFile(filePath, buffer, err => {
+         if (err) {
+           console.error(`Error writing file for image ${imageName}:`, err);
+         } else {
+           console.log(`Image saved for image ${imageName} at ${filePath}`);
+         }
+       });
+     }
+  }
+
+  if (req.files) {
+    const file = req.files.upload;
+    const filePath = path.join(__dirname, 'static', 'pogs', `${file.name}`);
+
+    file.mv(filePath, err => {
+      if (err) {
+        console.error('Error moving file:', err);
+        return res.status(500).send(err);
+      } else {
+        console.log(`File moved to ${filePath}`);
+      }
+    });
+  }
 
   var sql = 'UPDATE pogs SET color = ?, amount = ?, serial = ?, tags = ?, lore = ? WHERE uid = ?';
   db.run(sql, [color, amount, serial, tags, lore, uid], (err) => {
@@ -286,10 +268,14 @@ app.post('/savePog', (req, res) => {
       console.log('Database error:', err);
       res.status(500).json({ error: 'Database error' });
     } else {
-      res.json({ message: 'Data saved successfully' });
+      console.log(`Data and file saved successfully for pog with UID ${uid}`);
+      res.json({ message: 'Data and file saved successfully' });
     }
   });
 });
+
+
+
 
 
 
@@ -302,10 +288,9 @@ process.on('SIGINT', () => {
     console.log('Closed the database connection.');
     process.exit(0);
   })
-})
+});
 
 //Listens for connections on the specified port
 app.listen(PORT, () => {
-  console.log(`You're running on port ${PORT}.`);
-
-});
+  console.log(`You're running on port ${PORT}.`)
+})
